60256f21ac480da45083f92830dffaea0c54ffcd
igoryan-909 <igoryan909@gmail.com>
2014-07-22 13:30:49 +0300

    Демонстрационный блог на CodeIgniter

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -2,25 +2,37 @@
 
 class Blog extends CI_Controller
 {
+    // Массив данных, который передается в вид
     private $data = array(
                     'message' => ''
-                        ); // Массив данных, который передается в вид
+                        );
     
     public function __construct()
     {
         parent::__construct();
-        $this->lang->load('blog'); //Подгружаем языковой файл для блога
-        $this->load->model('Blog_model'); //Подгружаем модель для блога
-        $this->data['title']       = $this->Blog_model->get_setting('site_name'); // Получаем заголовок для блога по умолчанию
-        $this->data['description'] = ''; //Описание по умолчанию
+        
+        //Подгружаем языковой файл для блога
+        $this->lang->load('blog');
+        
+        //Подгружаем модель для блога
+        $this->load->model('Blog_model');
+        
+        // Получаем заголовок для блога по умолчанию
+        $this->data['title']       = $this->Blog_model->get_setting('site_name');
+        
+        //Описание по умолчанию
+        $this->data['description'] = '';
     }
+    
     /**
      * Главная страница - лента записей
+     * @param string $page
      */
     public function index($page = null)
     {
         // Библиотека пагинации CI
         $this->load->library('pagination');
+        
         //Формируем пагинацию
         $config['base_url']    = base_url() . __FUNCTION__;
         $config['total_rows']  = $this->Blog_model->total_posts();
@@ -33,8 +45,10 @@ class Blog extends CI_Controller
         $this->data['posts']      = $this->Blog_model->get_posts($config['per_page'], $page); //Формируем вывод записей в соответствии с параметрами пагинации
         $this->lib_view->output(array('main_header', 'blog/blog', 'main_footer'), $this->data); //Выводим вид
     }
+    
     /**
      * Страница записи
+     * @param string $post_url
      */
     public function post($post_url = null)
     {
@@ -46,12 +60,20 @@ class Blog extends CI_Controller
             {
                 //Подгружаем библиотеку валидации
                 $this->load->library('form_validation');
+                
                 //Правила для валидации
                 $this->form_validation->set_rules('post_comment_author', $this->lang->line('author'), 'required|no_tags|min_length[3]|max_length[30]');
         		$this->form_validation->set_rules('post_comment_content', $this->lang->line('comment'), 'required');
                 $this->form_validation->set_rules('post_comment_post_id', $this->lang->line('post_id'), 'required|post_exists');
+                
+                //Обрабатываем полученные post данные для отправки в модель
+                foreach($this->Blog_model->comment_fields as $key)
+                {
+                    $post_data[$key] = $this->input->post($key, TRUE);
+                }
+                
                 //Добавляем комментарий
-                if($this->Blog_model->add_comment())
+                if($this->Blog_model->add_comment($post_data))
                     $this->data['message'] = $this->lang->line('successfully_saved');
             }
             $this->data['comments'] = $this->Blog_model->get_comments(array('post_comment_post_id' => $this->data['post']->post_id));
@@ -60,17 +82,20 @@ class Blog extends CI_Controller
         else
             show_404();
     }
+    
     /**
      * Добавление записи
      */
     public function add_post()
     {
         $this->data['title'] = $this->lang->line('add_post') . ' | ' . $this->data['title'];
+        
         //Если форма отправлена
         if($this->input->post('submit'))
         {
             //Подгружаем библиотеку валидации
             $this->load->library('form_validation');
+            
             //Правила для валидации
             $this->form_validation->set_rules('post_author', $this->lang->line('author'), 'required|no_tags|min_length[3]|max_length[30]');
             $this->form_validation->set_rules('post_title', $this->lang->line('title'), 'required|no_tags|min_length[6]|max_length[100]');
@@ -78,14 +103,25 @@ class Blog extends CI_Controller
             $this->form_validation->set_rules('post_subject', $this->lang->line('subject'), 'required|no_tags|min_length[6]|max_length[150]');
     		$this->form_validation->set_rules('post_content', $this->lang->line('content'), 'required');
             $this->form_validation->set_rules('post_url', $this->lang->line('url'), 'required|no_tags|min_length[3]|max_length[30]');
+            
+            foreach($this->Blog_model->post_fields as $key)
+            {
+                $post_data[$key] = $this->input->post($key, TRUE);
+            }
+            
             //Если запись добавлена
-            if($this->Blog_model->add_post())
-                $this->lib_view->output(array('main_header', 'blog/send', 'main_footer'), $this->data); //Если данные сохранены успешно, выводим соответствующий вид
+            if($this->Blog_model->add_post($post_data))
+            
+                //Если данные сохранены успешно, выводим соответствующий вид
+                $this->lib_view->output(array('main_header', 'blog/send', 'main_footer'), $this->data);
             else
                 $this->lib_view->output(array('main_header', 'blog/add_post', 'main_footer'), $this->data);
-        } else //Иначе загружаем вид с формой
+                
+        //Иначе загружаем вид с формой
+        } else
             $this->lib_view->output(array('main_header', 'blog/add_post', 'main_footer'), $this->data);
     }
+    
     /**
      * Страница настроек блога
      */
@@ -95,12 +131,19 @@ class Blog extends CI_Controller
         {
             //Подгружаем библиотеку валидации
             $this->load->library('form_validation');
+            
             //Правила для валидации
             $this->form_validation->set_rules('site_name', $this->lang->line('site_name'), 'no_tags');
             $this->form_validation->set_rules('posts_per_page', $this->lang->line('posts_per_page'), 'required|no_tags|integer');
             $this->form_validation->set_rules('excerpt_characters', $this->lang->line('excerpt_characters'), 'required|integer');
             
-            if($this->Blog_model->save_settings())
+            //Обрабатываем полученные post данные для отправки в модель
+            foreach($this->Blog_model->main_settings_fields as $key)
+            {
+                $post_data[$key] = $this->input->post($key, TRUE);
+            }
+            
+            if($this->Blog_model->save_settings($post_data))
                 $this->data['message'] = $this->lang->line('successfully_saved');
         }
         $this->data['settings'] = $this->Blog_model->get_settings();
--- application/models/blog_model.php
+++ application/models/blog_model.php
@@ -4,8 +4,9 @@ class Blog_model extends CI_Model
 {
     //Указываем направление ORDER по умолчанию
     public $order_by = 'desc';
+    
     //Указываем поля, которые будут сохраняться в таблицах БД
-    private $post_fields = array(
+    public $post_fields = array(
                         'post_author',
                         'post_title',
                         'post_description',
@@ -13,12 +14,12 @@ class Blog_model extends CI_Model
                         'post_content',
                         'post_url'
                         );
-    private $comment_fields = array(
+    public $comment_fields = array(
                         'post_comment_post_id',
                         'post_comment_author',
                         'post_comment_content'
                         );
-    private $main_settings_fields = array(
+    public $main_settings_fields = array(
                         'site_name',
                         'posts_per_page',
                         'excerpt_characters'
@@ -28,8 +29,10 @@ class Blog_model extends CI_Model
     {
         parent::__construct();
     }
+    
     /**
      * Получаем значение определенного параметра ($settings) из таблицы установок в БД
+     * @return string
      */
     public function get_setting($setting)
     {
@@ -37,8 +40,10 @@ class Blog_model extends CI_Model
         $query = $this->db->get_where(SETTINGS_TABLE, $where);
         return $query->row()->setting_value;
     }
+    
     /**
      * Получаем все посты
+     * @return object
      */
     public function get_posts($limit = 5, $limit_offset = 0)
     {
@@ -47,107 +52,121 @@ class Blog_model extends CI_Model
         $query = $this->db->get_where(POSTS_TABLE);
         return $query->result();
     }
+    
     /**
      * Получаем общее количество записей
+     * @return string
      */
     public function total_posts()
     {
         return $this->db->count_all_results(POSTS_TABLE);
     }
+    
     /**
      * Получаем одну запись
+     * @return object
      */
     public function get_post($where)
     {
         $query = $this->db->get_where(POSTS_TABLE, $where);
         return $query->row();
     }
+    
     /**
      * Добавляем запись
+     * @return integer
      */
-    public function add_post()
+    public function add_post($post_data)
     {
         if($this->form_validation->run())
         {
-            foreach($this->post_fields as $key)
-            {
-                $data[$key] = $this->input->post($key);
-            }
-            $data['post_add_date'] = time();
-            if($this->db->insert(POSTS_TABLE, $data))
+            $post_data['post_add_date'] = time();
+            if($this->db->insert(POSTS_TABLE, $post_data))
                 return $this->db->insert_id();
         }
     }
+    
     /**
      * Получаем комментарии в зависимости от условий $where
+     * @return object
      */
     public function get_comments($where = null)
     {
         $query = $this->db->get_where(POST_COMMENTS_TABLE, $where);
         return $query->result();
     }
+    
     /**
      * Добавляем комментарий
+     * @return integer
      */
-    public function add_comment()
+    public function add_comment($post_data)
     {
-        if($this->form_validation->run()) //Если валидация прошла успешно
+        //Если валидация прошла успешно
+        if($this->form_validation->run())
         {
-            //Готовим данные для занесения в БД
-            foreach($this->comment_fields as $key)
-            {
-                $data[$key] = $this->input->post($key);
-            }
-            $data['post_comment_add_date'] = time(); // Текущее время
-            if($this->db->insert(POST_COMMENTS_TABLE, $data))
+            $post_data['post_comment_add_date'] = time(); // Текущее время
+            if($this->db->insert(POST_COMMENTS_TABLE, $post_data))
                 return $this->db->insert_id();
         }
     }
+    
     /**
      * Получаем кол-во комментариев в зависимости от условий
+     * @param mixed $where
+     * @return string
      */
     public function total_comments($where)
     {
         $this->db->where($where);
         return $this->db->count_all_results(POST_COMMENTS_TABLE);
     }
+    
     /**
      * Получаем и формируем в виде объекта настройки для блога из БД.
      * Настройки хранятся в БД в виде поле "ключ" -> поле "значение",
      * т.е. обращаясь к полю "ключ" мы получаем еще только ключ, а не сразу значение.
      * Чтобы не отходить от стандарта, формируем именно в виде объекта,
      * т.к. все остальные получаемые данные формируются тоже в виде объектов
+     * @return object
      */
     public function get_settings()
     {
         $where = array('setting_type' => 'blog');
         $query = $this->db->get_where(SETTINGS_TABLE, $where);
+        
+        //Указываем, что settings - это объект
+        $settings = new stdClass();
         foreach($query->result() as $col)
         {
             $settings->{$col->setting_parameter} = $col->setting_value;
         }
         return $settings;
     }
+    
     /**
      * Сохраняем настройки
+     * @return boolean
      */
-    public function save_settings()
+    public function save_settings($post_data)
     {
-        if($this->form_validation->run()) //Если валидация прошла успешно
+        //Если валидация прошла успешно, сохраняем настройки
+        if($this->form_validation->run())
         {
             $where = array('setting_type' => 'blog');
             $status = TRUE;
             foreach($this->main_settings_fields as $key)
             {
                 $where['setting_parameter'] = $key;
-                $data['setting_value'] = $this->input->post($key);
+                $data['setting_value'] = $post_data[$key];
                 if(!$this->db->update(SETTINGS_TABLE, $data, $where))
                 {
                     $status = FALSE;
                     break;
                 }
             }
-            return $status;
-        }
+        } else
+            $status = FALSE;
+        return $status;
     }
 }
